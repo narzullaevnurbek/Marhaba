93e64a6024789846afe6485c1f74cd1acc376660
narzullaevnurbek <narzullaevnurbekdev@gmail.com>
2023-06-19 14:28:14 +0500

    Initial Commit

---
+++ MarhabaCleanSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarhabaCleanSwift
+//
+//  Created by Narzullaev Nurbek on 19/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
